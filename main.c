b53b6747c1f46fa9e7d050c3f401c7c79051e7b2
chaebkimm <chae.b.kim@gmail.com>
2025-03-24 13:29:52 +0900

    Update main.c

--- main.c
+++ main.c
@@ -11,11 +11,11 @@
  * 배열에 초기값을 정하려면 { } 안에 초기값을 쉼표로 분리해서 적습니다. 
  */
 
-int a = 0xaaaaaaaa;
-int b = 0xbbbbbbbb;
-int array[2] = {0xab, 0xc0}; /* To Do: int 배열 array의 크기를 적절하게 바꿔서 메모리 창에서 a와 c, b와 d가 같은 세로줄에 있게 만들어보세요 */
-int c = 0xcccccccc;
-int d = 0xdddddddd;
+unsigned int a = 0xaaaaaaaa;
+unsigned int b = 0xbbbbbbbb;
+unsigned int array[2] = {0xab, 0xc0}; /* To Do: int 배열 array의 크기를 적절하게 바꿔서 메모리 창에서 a와 c, b와 d가 같은 세로줄에 있게 만들어보세요 */
+unsigned int c = 0xcccccccc;
+unsigned int d = 0xdddddddd;
 
 int main() {
   return 0;
